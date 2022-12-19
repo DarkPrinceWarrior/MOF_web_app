1a3b2eee3bb0f2f5f9a15329817b6289b6b1ae16
Ruslan <66064451+DarkPrinceWarrior@users.noreply.github.com>
2022-12-20 01:40:35 +0300

    pre-deploy state

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 import base64
+import pickle
 
+import numpy as np
 import streamlit
+from pydantic import BaseModel
 from streamlit_option_menu import option_menu
 import requests
 import pandas as pd
@@ -11,6 +14,14 @@ from components.page_database import excel_action
 from components.page_information import project_action
 from components.page_plots import plots_action
 from components.page_mof_information import mof_inf_action
+
+pkl_filename = "saved_models/Multi_RegressionModel.pkl"
+with open(pkl_filename, 'rb') as file1:
+    model = pickle.load(file1)
+
+pkl_filename_2 = "saved_models/saved_id2cat.pkl"
+with open(pkl_filename_2, 'rb') as file2:
+    id2cat = pickle.load(file2)
 
 streamlit.set_page_config(
     layout="wide",
@@ -43,6 +54,38 @@ background-size: cover;
 """
 
 streamlit.markdown(page_back, unsafe_allow_html=True)
+
+
+
+def predict(data):
+    new_data = np.array([data["Temp_reg_C"],
+                         data["W0_cm3_g"],
+                         data["E0_KDG_moll"],
+                         data["x0_nm"],
+                         data["a0_mmoll_gr"],
+                         data["E_kDg_moll"],
+                         data["SBAT_m2_gr"],
+                         data["Ws_cm3_gr"],
+                         data["Sme_m2_gr"],
+                         data["Wme_cm3_gr"]]).reshape(1, 10)
+
+    data_to_predict = pd.DataFrame(columns=['Tрег, ᵒС', 'W0, см3/г', 'E0, кДж/ моль', 'х0, нм', 'а0, ммоль/г',
+                                            'E,  кДж/моль', 'SБЭТ, м2/г', 'Ws, см3/г', 'Sme, м2/г', 'Wme, см3/г'],
+                                   data=new_data)
+    result = model.predict(data_to_predict)
+
+    list_of_cats = ["Металл", "Лиганд", "Растворитель"]
+    prediction_column_names = ['Металл', 'Лиганд', 'Растворитель', 'm (соли), г', 'm(кис-ты), г',
+                               'Vсин. (р-ля), мл', 'Т.син., °С', 'Vпр. (р-ля), мл', 'Т суш., °С',
+                               'm образца']
+
+    result.columns = prediction_column_names
+    for cat in list_of_cats:
+        result.loc[:, cat] = result[cat].apply(lambda x: id2cat[cat][round(x)])
+
+    prediction = {key: value for key, value in zip(prediction_column_names, result.values[0])}
+    # get the correct answer with decoded column values
+    return prediction
 
 
 def predict_action():
@@ -84,8 +127,10 @@ def predict_action():
         "Wme_cm3_gr": Wme_cm3_gr
     }
     if streamlit.button("Предсказать"):
-        response = requests.post("http://127.0.0.1:8000/predict", json=data)
-        prediction_table = response.json()
+        # response = requests.post("http://127.0.0.1:8000/predict", json=data)
+        # prediction_table = response.json()
+
+        prediction_table = predict(data)
         streamlit.title("Сгенерированные ИИ параметры синтеза МОК:")
         streamlit.table(pd.DataFrame(prediction_table, index=[0]))
 
@@ -94,7 +139,7 @@ def run():
     with streamlit.sidebar:
         selected = option_menu(
             menu_title="ИИ синтез адсорбентов",
-            options=["Наш проект", "МОК информация","Получить МОК", "Контакты", "База данных",
+            options=["Наш проект", "МОК информация", "Получить МОК", "Контакты", "База данных",
                      "Интерактив по МОК"],
             icons=["house", "book", "box fill", "person lines fill",
                    "clipboard data fill", "bar-chart-line-fill"],
--- main.py
+++ main.py
@@ -1,89 +1,89 @@
-import pickle
-
-from fastapi import FastAPI, Request
-from fastapi.responses import HTMLResponse
-from fastapi.staticfiles import StaticFiles
-from fastapi.templating import Jinja2Templates
-from pydantic import BaseModel
-import pandas as pd
-import numpy as np
-
-app = FastAPI()
-
-# don't need while used streamlit
-# app.mount("/static", StaticFiles(directory="static"), name="static")
-# templates = Jinja2Templates(directory="templates")
-
-pkl_filename = "app/saved_models/Multi_RegressionModel.pkl"
-with open(pkl_filename, 'rb') as file1:
-    model = pickle.load(file1)
-
-pkl_filename_2 = "app/saved_models/saved_id2cat.pkl"
-with open(pkl_filename_2, 'rb') as file2:
-    id2cat = pickle.load(file2)
-
-
-class Data(BaseModel):
-    """adsorbent information according to СЭХ
-
-    Tрег, ᵒС <class 'numpy.int64'>
-    W0, см3/г <class 'numpy.float64'>
-    E0, кДж/ моль <class 'numpy.float64'>
-    х0, нм <class 'numpy.float64'>
-    а0, ммоль/г <class 'numpy.float64'>
-    E,  кДж/моль <class 'numpy.float64'>
-    SБЭТ, м2/г <class 'numpy.float64'>
-    Ws, см3/г <class 'numpy.float64'>
-    Sme, м2/г <class 'float'>
-    Wme, см3/г <class 'numpy.float64'>
-    """
-
-    Temp_reg_C: int
-    W0_cm3_g: float
-    E0_KDG_moll: float
-    x0_nm: float
-    a0_mmoll_gr: float
-    E_kDg_moll: float
-    SBAT_m2_gr: float
-    Ws_cm3_gr: float
-    Sme_m2_gr: float
-    Wme_cm3_gr: float
-
-
-@app.get("/")
-async def root(request: Request):
-    return "Hello world!!!"
-    # return templates.TemplateResponse("index.html",
-    #                                   {"request": request})
-
-
-@app.post("/predict")
-def predict(data: Data):
-    new_data = np.array([data.Temp_reg_C,
-                       data.W0_cm3_g,
-                       data.E0_KDG_moll,
-                       data.x0_nm,
-                       data.a0_mmoll_gr,
-                       data.E_kDg_moll,
-                       data.SBAT_m2_gr,
-                       data.Ws_cm3_gr,
-                       data.Sme_m2_gr,
-                       data.Wme_cm3_gr]).reshape(1, 10)
-
-    data_to_predict = pd.DataFrame(columns=['Tрег, ᵒС', 'W0, см3/г', 'E0, кДж/ моль', 'х0, нм', 'а0, ммоль/г',
-       'E,  кДж/моль', 'SБЭТ, м2/г', 'Ws, см3/г', 'Sme, м2/г', 'Wme, см3/г'],
-                                   data=new_data)
-    result = model.predict(data_to_predict)
-
-    list_of_cats = ["Металл", "Лиганд", "Растворитель"]
-    prediction_column_names = ['Металл', 'Лиганд', 'Растворитель', 'm (соли), г', 'm(кис-ты), г',
-       'Vсин. (р-ля), мл', 'Т.син., °С', 'Vпр. (р-ля), мл', 'Т суш., °С',
-       'm образца']
-
-    result.columns = prediction_column_names
-    for cat in list_of_cats:
-        result.loc[:, cat] = result[cat].apply(lambda x: id2cat[cat][round(x)])
-
-    prediction = {key:value for key,value in zip(prediction_column_names,result.values[0])}
-    # get the correct answer with decoded column values
-    return prediction
+# import pickle
+#
+# from fastapi import FastAPI, Request
+# from fastapi.responses import HTMLResponse
+# from fastapi.staticfiles import StaticFiles
+# from fastapi.templating import Jinja2Templates
+# from pydantic import BaseModel
+# import pandas as pd
+# import numpy as np
+#
+# app = FastAPI()
+#
+# # don't need while used streamlit
+# # app.mount("/static", StaticFiles(directory="static"), name="static")
+# # templates = Jinja2Templates(directory="templates")
+#
+# pkl_filename = "saved_models/Multi_RegressionModel.pkl"
+# with open(pkl_filename, 'rb') as file1:
+#     model = pickle.load(file1)
+#
+# pkl_filename_2 = "saved_models/saved_id2cat.pkl"
+# with open(pkl_filename_2, 'rb') as file2:
+#     id2cat = pickle.load(file2)
+#
+#
+# class Data(BaseModel):
+#     """adsorbent information according to СЭХ
+#
+#     Tрег, ᵒС <class 'numpy.int64'>
+#     W0, см3/г <class 'numpy.float64'>
+#     E0, кДж/ моль <class 'numpy.float64'>
+#     х0, нм <class 'numpy.float64'>
+#     а0, ммоль/г <class 'numpy.float64'>
+#     E,  кДж/моль <class 'numpy.float64'>
+#     SБЭТ, м2/г <class 'numpy.float64'>
+#     Ws, см3/г <class 'numpy.float64'>
+#     Sme, м2/г <class 'float'>
+#     Wme, см3/г <class 'numpy.float64'>
+#     """
+#
+#     Temp_reg_C: int
+#     W0_cm3_g: float
+#     E0_KDG_moll: float
+#     x0_nm: float
+#     a0_mmoll_gr: float
+#     E_kDg_moll: float
+#     SBAT_m2_gr: float
+#     Ws_cm3_gr: float
+#     Sme_m2_gr: float
+#     Wme_cm3_gr: float
+#
+#
+# @app.get("/")
+# async def root(request: Request):
+#     return "Hello world!!!"
+#     # return templates.TemplateResponse("index.html",
+#     #                                   {"request": request})
+#
+#
+# @app.post("/predict")
+# def predict(data: Data):
+#     new_data = np.array([data.Temp_reg_C,
+#                        data.W0_cm3_g,
+#                        data.E0_KDG_moll,
+#                        data.x0_nm,
+#                        data.a0_mmoll_gr,
+#                        data.E_kDg_moll,
+#                        data.SBAT_m2_gr,
+#                        data.Ws_cm3_gr,
+#                        data.Sme_m2_gr,
+#                        data.Wme_cm3_gr]).reshape(1, 10)
+#
+#     data_to_predict = pd.DataFrame(columns=['Tрег, ᵒС', 'W0, см3/г', 'E0, кДж/ моль', 'х0, нм', 'а0, ммоль/г',
+#        'E,  кДж/моль', 'SБЭТ, м2/г', 'Ws, см3/г', 'Sme, м2/г', 'Wme, см3/г'],
+#                                    data=new_data)
+#     result = model.predict(data_to_predict)
+#
+#     list_of_cats = ["Металл", "Лиганд", "Растворитель"]
+#     prediction_column_names = ['Металл', 'Лиганд', 'Растворитель', 'm (соли), г', 'm(кис-ты), г',
+#        'Vсин. (р-ля), мл', 'Т.син., °С', 'Vпр. (р-ля), мл', 'Т суш., °С',
+#        'm образца']
+#
+#     result.columns = prediction_column_names
+#     for cat in list_of_cats:
+#         result.loc[:, cat] = result[cat].apply(lambda x: id2cat[cat][round(x)])
+#
+#     prediction = {key:value for key,value in zip(prediction_column_names,result.values[0])}
+#     # get the correct answer with decoded column values
+#     return prediction
